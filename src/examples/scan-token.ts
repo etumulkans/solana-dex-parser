670943e8fd8b13922c58efe0d560e0dd117083a2
Ervins Tumulkans <ervins.tumulkans@tet.ov>
2025-04-12 22:33:51 +0300

    dd

--- src/examples/scan-token.ts
+++ src/examples/scan-token.ts
@@ -1,7 +1,7 @@
 import { TokenScanner } from '../scanner/token-scanner';
 
 async function main() {
-  const TOKEN_ADDRESS = 'ALbGvvupsSS6sA8xjQstVaRtHaUuoxwgo7Jfjcb7YD9G';
+  const TOKEN_ADDRESS = '8wxqW3Twx3vXGn21j4VqEbNngEK9DT2eZ1ajhRfhpump';
   const scanner = new TokenScanner(TOKEN_ADDRESS);
   await scanner.startScanning();
 }
