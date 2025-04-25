7a53c849f9150f854925737436dcc21443145591
Ervins Tumulkans <ervins.tumulkans@tet.ov>
2025-04-25 21:25:37 +0300

    dd

--- src/examples/scan-token.ts
+++ src/examples/scan-token.ts
@@ -1,10 +1,10 @@
 import { TokenScanner } from '../scanner/token-scanner';
 
 async function main() {
-  const TOKEN_ADDRESS = 'Ht5BNRYc1ho2vi6a82TR8fngSC268MRncVFqcW3LZY3g'; 
+  const TOKEN_ADDRESS = '5FGiPVb9RK6Xz9a6qiEVYaCFmteSzcp9SB73pD8AHAvg';
   //      Ht5BNRYc1ho2vi6a82TR8fngSC268MRncVFqcW3LZY3g //2TBpuWr1at8wu7edguKebFVV9BrvbuMjTRwp34H38SV8
-  const SCAN_ADDRESS = "2TBpuWr1at8wu7edguKebFVV9BrvbuMjTRwp34H38SV8";
-  const scanner = new TokenScanner(TOKEN_ADDRESS,SCAN_ADDRESS);
+  const SCAN_ADDRESS = '2oiSu4BSXJ6BB99WXpZRghR4JXzbGF5oLAiRxAReAXqi';
+  const scanner = new TokenScanner(TOKEN_ADDRESS, SCAN_ADDRESS);
   await scanner.startScanning();
 }
 
