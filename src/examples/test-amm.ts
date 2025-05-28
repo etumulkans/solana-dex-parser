359429d5d0b11ebe9c9917ba208950f7d79399a9
Ervins Tumulkans <ervins.tumulkans@tet.ov>
2025-05-28 19:38:44 +0300

    dd

--- src/examples/test-amm.ts
+++ src/examples/test-amm.ts
@@ -1,9 +1,9 @@
 import { TokenScanner } from '../scanner/amm-grpc-test';
 
 async function main() {
-  const TOKEN_ADDRESS = 'HueN6T2L2gZPUoEBBodaJS6ytXXHUWnmE4ii8nKfoZjF';
+  const TOKEN_ADDRESS = '7fUbHvZwb3Rj2BC195FwhmFBa9HUYHWZcyqHHWy5pump';
   //      Ht5BNRYc1ho2vi6a82TR8fngSC268MRncVFqcW3LZY3g //2TBpuWr1at8wu7edguKebFVV9BrvbuMjTRwp34H38SV8
-  const SCAN_ADDRESS = 'BhdcKQeYDQZrvyLVTktB4vFpRkh6KQCbNrsDkiWLtzn5';
+  const SCAN_ADDRESS = '7fUbHvZwb3Rj2BC195FwhmFBa9HUYHWZcyqHHWy5pump';
   const scanner = new TokenScanner(TOKEN_ADDRESS, SCAN_ADDRESS);
   await scanner.startScanning();
 }
