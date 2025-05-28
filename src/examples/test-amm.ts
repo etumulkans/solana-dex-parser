083c2d288865d832a93d9b36a23a086ab5ffac08
Ervins Tumulkans <ervins.tumulkans@tet.ov>
2025-05-28 19:44:02 +0300

    ss

--- src/examples/test-amm.ts
+++ src/examples/test-amm.ts
@@ -1,9 +1,9 @@
 import { TokenScanner } from '../scanner/amm-grpc-test';
 
 async function main() {
-  const TOKEN_ADDRESS = '7fUbHvZwb3Rj2BC195FwhmFBa9HUYHWZcyqHHWy5pump';
+  const TOKEN_ADDRESS = 'pAMMBay6oceH9fJKBRHGP5D4bD4sWpmSwMn52FMfXEA';
   //      Ht5BNRYc1ho2vi6a82TR8fngSC268MRncVFqcW3LZY3g //2TBpuWr1at8wu7edguKebFVV9BrvbuMjTRwp34H38SV8
-  const SCAN_ADDRESS = '2oN7rnVAExkobt7CbmT7obLEwnjFy6FfpiuAuU2e1Epk';
+  const SCAN_ADDRESS = 'pAMMBay6oceH9fJKBRHGP5D4bD4sWpmSwMn52FMfXEA';
   const scanner = new TokenScanner(TOKEN_ADDRESS, SCAN_ADDRESS);
   await scanner.startScanning();
 }
